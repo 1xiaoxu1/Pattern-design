64522a2ec5e2f1b97f58ea57ab752116a7354b70
徐 <1902882745@qq.com>
2023-08-01 16:07:46 +0800

    refactor(dist/BackToTop)重构代码

--- cimi/dist/BackToTop/index.d.ts
+++ cimi/dist/BackToTop/index.d.ts
@@ -1,14 +1,4 @@
-import React from 'react';
-export interface ScrollToTopProps {
-    /**
-     * @description 向下滑动的值
-     */
-    showUnder?: number;
-    /**
-     * @description css样式
-     */
-    style: React.CSSProperties;
-    onClick?: () => void;
-}
-declare const ScrollToTop: React.FC<ScrollToTopProps>;
-export default ScrollToTop;
+import { FC } from 'react';
+import { BackToTopProps } from './interface';
+declare const BackToTop: FC<BackToTopProps>;
+export default BackToTop;
